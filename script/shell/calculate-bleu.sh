9da5c9a0e86f1ea12837173472bb37c91ce3e12e
suzuhiki <suzuhiki@gmail.com>
2023-05-05 22:49:11 +0900

    学習成功

--- script/shell/calculate-bleu.sh
+++ script/shell/calculate-bleu.sh
@@ -2,6 +2,6 @@
 # required : multi-bleu.perl included in moses
 multi_bleu="/home/morioka/util/mosesdecoder/scripts/generic/multi-bleu.perl"
 ref_path="/home/morioka/workspace/git_projects/lab-tutorial-nmt/resource/tokenized/test.en"
-hyp_path="/home/morioka/workspace/git_projects/lab-tutorial-nmt/data/test_output/lstm_s2s_24_0.035533253638445415.pth.en"
+hyp_path="/home/morioka/workspace/git_projects/lab-tutorial-nmt/data/test_output/lstm_s2s_24_0.02667330542932727.pth.en"
 
 perl ${multi_bleu} -lc ${ref_path} < ${hyp_path} 
